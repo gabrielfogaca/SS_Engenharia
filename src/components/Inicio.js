a23f605de75627b10137ccf726446ce56e18f309
unknown <gabrielfurlanfogaca@gmail.com>
2024-09-06 17:33:44 -0300

    ajuste mobile

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import img18 from './img/img18.jpeg';
 import background from './img/background.jpeg';
 import logo2 from './img/logo2.jpeg';
@@ -24,17 +24,59 @@ const left = {
   },
 };
 
+function getWindowSize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  return { width, height };
+}
+
 function Inicio() {
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize(getWindowSize());
+    };
+
+    // Adiciona o listener para o evento de resize da janela
+    window.addEventListener('resize', handleResize);
+
+    // Remove o listener quando o componente for desmontado
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
-    <div
-      className="flex justify-center items-center bg-fixed bg-no-repeat bg-center bg-cover h-screen"
-      style={{
-        backgroundImage: `url(${background})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-      }}
-    ></div>
+    <>
+      {windowSize.width < 740 ? (
+        <div
+          className="flex justify-center items-center bg-fixed bg-no-repeat bg-cover min-h-screen static"
+          style={{
+            backgroundImage: `url(${background})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundAttachment: 'fixed',
+            width: '100vw',
+            height: '100vh',
+          }}
+        ></div>
+      ) : (
+        <div
+          className="flex justify-center items-center bg-fixed bg-no-repeat bg-center bg-cover h-screen"
+          style={{
+            backgroundImage: `url(${background})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'left',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          <div>
+            <p>Largura da janela: {windowSize.width}px</p>
+            <p>Altura da janela: {windowSize.height}px</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
