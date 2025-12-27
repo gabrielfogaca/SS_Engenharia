e7b47bc4e407e0562c1add2fbdb615d9b67c4f78
Gabriel F. Fogaça <gabrielfurlanfogaca@gmail.com>
2025-12-27 22:49:59 +1100

    ignore dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Configurado para o repositório: SS_Engenharia
-  base: '/SS_Engenharia/',
+  base: '/',
   
   plugins: [react(), tailwindcss()],
   resolve: {
