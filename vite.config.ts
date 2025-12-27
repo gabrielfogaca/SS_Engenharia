7d7efac6896e2aecd54b1f38cf49e6a4ac50a734
Gabriel F. Fogaça <gabrielfurlanfogaca@gmail.com>
2025-12-27 22:55:31 +1100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/',
-  
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
