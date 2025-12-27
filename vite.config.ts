df80df658981ac7d437f11c3f86109c952cdd03b
Gabriel F. Fogaça <gabrielfurlanfogaca@gmail.com>
2025-12-27 21:07:30 +1100

    config gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,14 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/SS_Engenharia/',
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const isGhPages = process.env.GITHUB_PAGES === 'true'
+
+  return {
+    base: isGhPages ? '/SS_Engenharia/' : '/',
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: { '@': path.resolve(__dirname, './src') },
     },
-  },
+  }
 })
